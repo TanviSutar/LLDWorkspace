7c2d947343ba8b99ef4a10466bfbc5267454e744
Tanvi Sutar <tanvi@Tanvis-MacBook-Pro.local>
2022-11-30 19:59:38 +0530

    Added singleton example

---
+++ DesignPatterns/Singleton/Logger.java
@@ -0,0 +1,23 @@
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+public class Logger{
+    private volatile static Logger logger;
+    private Logger(){}
+    public static Logger getLogger(){
+        if(logger == null){
+            synchronized(Logger.class){
+                if(logger == null){
+                    System.out.println("Logger instantiated");
+                    logger = new Logger();
+                }
+            }
+        }
+        return logger;
+    }
+    public void log(String message){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime current_time = LocalDateTime.now();
+        System.out.println(formatter.format(current_time)+": "+ message);
+    }
+}
---
+++ DesignPatterns/Singleton/LoggerExample.java
@@ -0,0 +1,22 @@
+
+
+public class LoggerExample{
+    public static void main(String[] args) throws InterruptedException{
+        Thread threadOne = new Thread() {
+            public void run(){
+                Logger logger = Logger.getLogger();
+                logger.log("Logs from thread one.");
+            }
+        };
+        Thread threadTwo = new Thread() {
+            public void run() {
+                Logger logger = Logger.getLogger();
+                logger.log("Logs from thread two.");
+            }
+        };
+        threadOne.start();
+        threadTwo.start();
+        threadOne.join();
+        threadTwo.join();
+    }
+}
